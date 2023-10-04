cf362a676550973c220241bdf237fdf3fb59b4b9
wahujos <jwahu30@gmail.com>
2023-10-04 08:41:59 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for number in range(99):
-	formatted = "{:02d}".format(number)
-	print("{}, ".format(formatted), end='')
+    formatted = "{:02d}".format(number)
+    print("{}, ".format(formatted), end='')
 print('99')
