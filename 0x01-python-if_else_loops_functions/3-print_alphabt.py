e2d0a87294f68f4ab2469d381873d992f7b7424f
wahujos <jwahu30@gmail.com>
2023-10-04 12:12:26 +0000

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in 'abcdefghijklmnopqrstuvwzyz':
+for letter in 'abcdefghijklmnopqrstuvwxyz':
     if letter == 'q' or letter == 'e':
         continue
     print("{}".format(letter), end='')
