19a811eb4412621cff2fd4810a662f4d05cb5b2b
wahujos <jwahu30@gmail.com>
2023-10-05 23:21:43 +0000

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+#if __name__ == "__main__":
+def calculate_sum():
+    total = 0
+    num_args = len(sys.argv) - 1
+    for num in range(num_args):
+        total += int(sys.argv[num + 1])
+    return total
+
+if __name__ == "__main__":
+    result = calculate_sum()
+    print(result)
