57fd72f3bfdeaa75b659ba69bdee9f1e91470a51
wahujos <jwahu30@gmail.com>
2023-10-06 00:00:25 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    dir(hidden_4)
+    names_defined = dir(hidden_4)
+    for name in range(names_defined):
+        if name[0] == '_':
+            continue
+        else:
+            print({}.format(name))
