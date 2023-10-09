416b30bd0db04092cda21bc799b85365325a53ee
wahujos <jwahu30@gmail.com>
2023-10-09 08:55:56 +0000

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for interger in my_list:
-            print("{}".format(interger))
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
