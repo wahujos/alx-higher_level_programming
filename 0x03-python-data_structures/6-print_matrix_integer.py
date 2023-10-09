ecf110b0efa1430e23a8b0e71d364604d9db942a
wahujos <jwahu30@gmail.com>
2023-10-09 14:37:29 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for row in matrix:
         for num in row:
             if row.index(num) == (len(row) - 1):
