4ad342aefef93a20751d25246ffd2f316cd68439
wahujos <jwahu30@gmail.com>
2023-10-09 12:18:41 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    result = ""
     for letter in my_string:
-        if letter == 'c' or letter == 'C':
-            continue
-        print("{}".format(letter), end='')
+        if letter != 'c' or letter != 'C':
+            result += letter
+    return result
