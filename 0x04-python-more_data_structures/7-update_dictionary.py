fbba8245f45fd0f4755ecbdd222e6b7983a5dd12
wahujos <jwahu30@gmail.com>
2023-10-11 21:01:27 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key in a_dictionary:
-        if key in a_dictionary:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
+    a_dictionary[key] = value
