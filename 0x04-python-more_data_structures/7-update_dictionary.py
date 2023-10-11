7dd9f9df05f9826e23778720f0d41285f618a9e5
wahujos <jwahu30@gmail.com>
2023-10-11 21:52:18 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for value in a_dictionary:
+            if value == key:
+                a_dictionary[key] = value
+    return a_dictionary
