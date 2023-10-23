75c6ca6e30973b7eb6a1cf46208b95d7e33d3fc8
wahujos <jwahu30@gmail.com>
2023-10-23 16:49:18 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if(isinstance(value, int)):
+        if (isinstance(value, int)):
             print("{:d}".format(value))
             return True
         else:
