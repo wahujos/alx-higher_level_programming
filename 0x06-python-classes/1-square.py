a68b46e688eb0aff4648d1d12133a2ff84bc397b
wahujos <jwahu30@gmail.com>
2023-10-24 19:38:57 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' creating a python class called square '''
+
+
+class Square:
+    ''' init method associated by the class.
+    This is the method that runs or rather
+    its executed once we instatiate the class'''
+
+    def __init__(self, size):
+        self.size = size
