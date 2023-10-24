c03f6a143012ebfc1298f456675006689bf42a0c
wahujos <jwahu30@gmail.com>
2023-10-24 23:45:45 +0000

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''Module begins'''
+
+
+class Square:
+    ''' created a class'''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not (isinstance(value, tuple) and len(value) == 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            x, y = self.__position
+            for i in range(y):
+                print()
+            for i in range(self.__size):
+                print(" " * x + "#" * self.__size)
