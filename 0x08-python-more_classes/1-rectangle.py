428828a0c85e0185730b9caca68af3854ec8ed3f
wahujos <jwahu30@gmail.com>
2023-10-30 15:44:46 +0000

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''A class Rectangle'''
+
+
+class Rectangle:
+    '''Defining class rectangle'''
+    def __init__(self, width=0, height=0):
+        '''initializing the init method'''
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        '''retrieve width property'''
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        '''Sets the width'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+    @property
+    def height(self):
+        '''retriving height'''
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        '''setting the height'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
