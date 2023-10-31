53d5c78237c77102c8c85910ff143c1de29e42d6
wahujos <jwahu30@gmail.com>
2023-10-31 21:42:20 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+doc string - defines an add integer function
+
+'''
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
