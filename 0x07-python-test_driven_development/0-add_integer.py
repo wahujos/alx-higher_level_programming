050509163125227c7531bf22476aa8805af85bc2
wahujos <jwahu30@gmail.com>
2023-11-04 02:27:50 +0000

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,6 +6,7 @@ doc string - defines an add integer function
 
 
 def add_integer(a, b=98):
+    '''documenting function'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
