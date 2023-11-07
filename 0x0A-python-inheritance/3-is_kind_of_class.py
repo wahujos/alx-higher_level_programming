a5839fa0aedc7a9f58b00214289bdd3a04f0785b
wahujos <jwahu30@gmail.com>
2023-11-07 04:58:56 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" define the module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """defining the function"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
