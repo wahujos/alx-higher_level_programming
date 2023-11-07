d06f3d13e93d0340b6afdb7fa9856f3adce5d3a8
wahujos <jwahu30@gmail.com>
2023-11-07 04:52:01 +0000

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define module"""
+
+
+def is_same_class(obj, a_class):
+    """defining the function"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
