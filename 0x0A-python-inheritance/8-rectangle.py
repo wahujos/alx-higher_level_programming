08d51a683644af4a6dae43575c0a4191d043ab91
wahujos <jwahu30@gmail.com>
2023-11-07 11:12:52 +0000

    update styleing

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,6 +15,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """class documentation"""
     def __init__(self, width, height):
