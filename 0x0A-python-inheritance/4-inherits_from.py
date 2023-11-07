6c9a0dd0b12df9206c51bffc0a33b5b07aa87a44
wahujos <jwahu30@gmail.com>
2023-11-07 05:42:54 +0000

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """defining function"""
-    if issubclass(type(obj), a_class)and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
