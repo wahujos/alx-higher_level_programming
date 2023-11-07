4e0c338010501e3b5928bc847b3f047ac3bd6285
wahujos <jwahu30@gmail.com>
2023-11-07 05:38:48 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining module"""
+
+
+def inherits_from(obj, a_class):
+    """defining function"""
+    if issubclass(type(obj), a_class)and type(obj) != a_class:
+        return True
+    else:
+        return False
