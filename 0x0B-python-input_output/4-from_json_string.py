5f8d56edc1d0f2bf6fb8d977b6ae2a91e0f8bf97
wahujos <jwahu30@gmail.com>
2023-11-08 21:03:37 +0000

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""documentation of modules"""
+import json
+"""handles json"""
+
+
+def from_json_string(my_str):
+    """defining the function"""
+    return json.loads(my_str)
