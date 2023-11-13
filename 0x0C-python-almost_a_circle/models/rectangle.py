ed063e2c77d172042cd82de4a7a386d5d693a25c
wahujos <jwahu30@gmail.com>
2023-11-13 15:21:43 +0000

    offset

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,25 +8,25 @@ class Rectangle(Base):
     """defining the class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        if not isinstance(width, int):
+        '''if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be >= 0")'''
         self.__width = width
-        if not isinstance(height, int):
+        '''if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")'''
         self.__height = height
-        if not isinstance(x, int):
+        '''if not isinstance(x, int):
             raise TypeError("x must be an integer")
         if x < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("x must be >= 0")'''
         self.__x = x
-        if not isinstance(y, int):
+        '''if not isinstance(y, int):
             raise TypeError("y must be an integer")
         if y < 0:
-            raise ValueError("y must be >= 0")
+            raise ValueError("y must be >= 0")'''
         self.__y = y
 
     @property
