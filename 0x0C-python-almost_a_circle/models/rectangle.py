1bc642ce6ef5e41c6b5e8d00b90b20b4570ce2d5
wahujos <jwahu30@gmail.com>
2023-11-14 19:25:36 +0000

    major update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''Handle modules and any other stuff'''
+"""Handle modules and any other stuff"""
+
+
 from models.base import Base
-'''Getting information about the base'''
 
 
 class Rectangle(Base):
     """defining the class Rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """defining the initialization method defining
-        the initialization method"""
+        the initialization method
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,12 +20,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -31,12 +34,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -45,12 +48,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         if not isinstance(value, int):
             raise TypeError("x must be an interger")
         if value < 0:
@@ -59,12 +62,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -77,20 +80,20 @@ class Rectangle(Base):
 
         Returns:
         - The area of the rectangle.
-
         """
         return self.__width * self.__height
 
     def display(self):
         """This is the function responsible for display of the shape in
-        different ways whether with or without offset."""
+        different ways whether with or without offset.
+        """
         for _ in range(self.y):
             print()
         for i in range(self.height):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         c_name = __class__.__name__
         s_id = self.id
         x1 = self.x
@@ -100,7 +103,7 @@ class Rectangle(Base):
         return "[{}] ({}) {}/{} - {}/{}".format(c_name, s_id, x1, y1, w1, h1)
 
     def update(self, *args, **kwags):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         if args:
             attributes_given = ['id', 'width', 'height', 'x', 'y']
             for i, value in enumerate(args):
@@ -110,7 +113,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """place holder documentation place holder documentation """
+        """place holder documentation place holder documentation"""
         return {'id': self.id,
                 'width': self.width,
                 'height': self.height,
