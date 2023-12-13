a662ea1df6c0e6ed791566dc9feea88db160f423
wahujos <jwahu30@gmail.com>
2023-12-14 02:17:39 +0300

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- script that inserts a new row in the table first_table
 -- New row: id = 89 name = Best School
-INSERT INTO first_table VALUES (89, Best School);
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
