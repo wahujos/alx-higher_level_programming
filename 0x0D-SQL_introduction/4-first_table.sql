e072640a2fcf12026df0bd0bb3fc62f72d7ca04b
wahujos <jwahu30@gmail.com>
2023-12-14 00:27:58 +0300

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- creates a table called first_table in the current database
+-- first_table description: id INT name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
