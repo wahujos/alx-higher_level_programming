cecb40fd2e3bb14c0fd69fc47eab6080b719fa7a
wahujos <jwahu30@gmail.com>
2024-01-10 18:37:13 +0300

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints 3 lines
+
+const astr = [];
+astr[0] = 'C is fun';
+astr[1] = 'Python is cool';
+astr[2] = 'JavaScript is amazing';
+for (let i = 0; i < astr.length; i++) {
+  console.log(astr[i]);
+}
