4c957410b046870d9fd3e7acc9aec16df0078569
wahujos <jwahu30@gmail.com>
2024-01-11 02:18:02 +0300

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // function that retuns the addition of two intergers
 
-function add(a, b) {
-	return (a + b);
+function add (a, b) {
+  return (a + b);
 }
 module.exports.add = add;
