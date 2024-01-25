1a864602f9a0dd8b329f21d32c46a804f48179a0
wahujos <jwahu30@gmail.com>
2024-01-25 15:02:14 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Technical interview algorithm"""
-# finds a peak in a list of unsorted integers
+"""
+Technical interview algorithm
+finds a peak in a list of unsorted integers
+"""
+
+
 def find_peak(list_of_integers):
     """Find peak function algorith"""
     high = len(list_of_integers) - 1
