c6b96a5d819b9d10ec13555455649da2b33a073a
wahujos <jwahu30@gmail.com>
2024-01-30 16:21:59 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,16 +8,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
     auth = (username, token)
-    headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            }
     res = requests.get('https://api.github.com/user',
-                       auth=auth, headers=headers)
+                       auth=auth)
     if res.status_code == 200:
         user_data = res.json()
         if user_data.get('id') is None:
             print("None")
         else:
-            print(f'{user_data[id]}')
+            print(f"{user_data.get('id')}")
     else:
         print(f"Error {res.status_code} {res.text}")
